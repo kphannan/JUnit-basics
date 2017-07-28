a55269e9c6aaa54a0cc78a70329d0a582fb1c7c5
Kevin P. Hannan <kphannan@gmail.com>
2017-07-28 00:31:07 -0400

    Added a main to use JavaCore.runClasses

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import org.junit.runner.JUnitCore;
+
+/**
+ * Created by kevin on 7/28/2017.
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        JUnitCore.runClasses( TestSuite_1.class );
+    }
+}
