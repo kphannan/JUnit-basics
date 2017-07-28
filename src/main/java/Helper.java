9877f5448fb66eb36ffd15ee31787235a3079ac0
Kevin P. Hannan <kphannan@gmail.com>
2017-07-28 09:20:58 -0400

    Cleaned up commented out code

--- src/main/java/Helper.java
+++ src/main/java/Helper.java
@@ -4,11 +4,9 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-//import jdk.nashorn.internal.parser.JSONParser;
 
 import java.io.FileReader;
 import java.io.IOException;
-//import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -21,9 +19,6 @@ public class Helper {
     public static Collection data() throws IOException, ParseException
     {
         List<Object[]> data    = new ArrayList<>();
-//        JSONParser     parser  = new JSONParser();
-//        Object         rawJson = parser.parse( new FileReader("foo"));
-//        Object[]       keys    = rawJson.keySet().toArray();
         JSONParser     parser  = new JSONParser();
         JSONObject     rawJson = (JSONObject)parser.parse( new FileReader("src/main/resources/test.json"));
         Object[]       keys    = rawJson.keySet().toArray();
