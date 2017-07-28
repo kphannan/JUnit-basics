b26242a95b339c1b0280269591f092fe58b0b06c
Kevin P. Hannan <kphannan@gmail.com>
2017-07-28 00:17:30 -0400

    Initial project

---
+++ src/main/java/TestSuite_1.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+/**
+ * Created by kevin on 7/28/2017.
+ */
+public class TestSuite_1 {
+
+    @Test
+    public void test1()
+    {
+        System.out.println( "TestSuite_1 Test 1");
+    }
+
+    @Test
+    public void test2()
+    {
+        System.out.println( "TestSuite_1 Test 2");
+    }
+}
