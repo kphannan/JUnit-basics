392bbe041e263aca4d5e218f1960b3958fba821d
Kevin P. Hannan <kphannan@gmail.com>
2017-07-28 00:25:02 -0400

    Added before/after test and class

--- src/main/java/TestSuite_1.java
+++ src/main/java/TestSuite_1.java
@@ -1,9 +1,21 @@
-import org.junit.Test;
+import org.junit.*;
 
 /**
  * Created by kevin on 7/28/2017.
  */
 public class TestSuite_1 {
+
+    @BeforeClass
+    public static void beforeClass()
+    {
+        System.out.println( "Before class");
+    }
+
+    @Before
+    public void beforeTest()
+    {
+        System.out.println( "Before test");
+    }
 
     @Test
     public void test1()
@@ -16,4 +28,19 @@ public class TestSuite_1 {
     {
         System.out.println( "TestSuite_1 Test 2");
     }
+
+
+    @After
+    public void afterTest()
+    {
+        System.out.println( "After test");
+    }
+
+    @AfterClass
+    public static void afterClass()
+    {
+        System.out.println( "After class");
+    }
+
+
 }
